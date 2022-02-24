2df62404d83300f6f9868848ea0c3ec4cfb678b1
Taweechai Yuenyang <krumii.it@gmail.com>
2022-02-24 16:41:12 +0700

    add footer

--- resources/js/Pages/Welcome.js
+++ resources/js/Pages/Welcome.js
@@ -175,7 +175,6 @@ const Welcome = (props) => {
         // --Container--
         <div className="container w-full mx-auto pt-20">
           <div className="w-full px-4 md:px-0 md:mt-8 mb-16 text-gray-800 leading-normal">
-            // --summary card--
             <div className="flex flex-wrap">
               <div className="w-full md:w-1/2 xl:w-1/3 p-3">
                 <div className="bg-white border rounded shadow p-2">
@@ -300,9 +299,58 @@ const Welcome = (props) => {
                 </div>
               </div>
             </div>
+            <hr className="border-b-2 border-gray-400 my-8 mx-4"></hr>
           </div>
         </div>
       </div>
+      <footer className="bg-white border-t border-gray-400 shadow">
+        <div className="container max-w-md mx-auto flex py-8">
+          <div className="w-full mx-auto flex flex-wrap">
+            <div className="flex w-full md:w-1/2 ">
+              <div className="px-8">
+                <h3 className="font-bold font-bold text-gray-900">About</h3>
+                <p className="py-4 text-gray-600 text-sm">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Maecenas vel mi ut felis tempus commodo nec id erat.
+                  Suspendisse consectetur dapibus velit ut lacinia.
+                </p>
+              </div>
+            </div>
+
+            <div className="flex w-full md:w-1/2">
+              <div className="px-8">
+                <h3 className="font-bold font-bold text-gray-900">Social</h3>
+                <ul className="list-reset items-center text-sm pt-3">
+                  <li>
+                    <a
+                      className="inline-block text-gray-600 no-underline hover:text-gray-900 hover:underline py-1"
+                      href="#"
+                    >
+                      Add social link
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      className="inline-block text-gray-600 no-underline hover:text-gray-900 hover:underline py-1"
+                      href="#"
+                    >
+                      Add social link
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      className="inline-block text-gray-600 no-underline hover:text-gray-900 hover:underline py-1"
+                      href="#"
+                    >
+                      Add social link
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
     </>
   )
 }
