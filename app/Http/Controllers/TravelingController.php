e1b8ae7d6b3f6a793f3bfd179e148d042f1b2594
Taweechai Yuenyang <krumii.it@gmail.com>
2022-02-25 10:39:28 +0700

    update add traveling

---
+++ app/Http/Controllers/TravelingController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Traveling;
+use Illuminate\Http\Request;
+
+class TravelingController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Traveling  $traveling
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Traveling $traveling)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Traveling  $traveling
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Traveling $traveling)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Traveling  $traveling
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Traveling $traveling)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Traveling  $traveling
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Traveling $traveling)
+    {
+        //
+    }
+}
---
+++ app/Models/Traveling.php
@@ -0,0 +1,20 @@
+<?php
+
+
+namespace App\Models;
+
+use App\Traits\Nanoids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Laravel\Sanctum\HasApiTokens;
+
+class Traveling extends Model
+{
+    use HasFactory, HasApiTokens, Nanoids;
+
+    public $fillable = [
+        'name',
+        'description',
+        'is_active',
+    ];
+}
